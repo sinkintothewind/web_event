2c6b9fd012127bec96ab4f4860606533d6a8f680
wanghong <1042322353@qq,com>
2024-03-11 21:39:51 +0800

    登陆和注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,52 @@
+$(function () {
+    // 登录/注册表单点击切换事件
+    $('#link_reg').on('click', function () {
+        $('.reg-box').show()
+        $('.login-box').hide()
+    })
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+// 表单验证规则
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) { return '两次密码不一致！' }
+        }
+    })
+    // 登陆表单提交发起ajax请求
+    $('#login-form').on('submit', function (e) {
+        e.prebentDefault()
+        var data = {
+            uesrname: $('#login-form[name=username]').val(),
+            password:$('#reg-form[name=password]').val()
+        }
+        $.ajax({
+            url: 'http://www.liulongbin.top:3007/api/login',
+            method: 'post',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+    return layer.msg('登陆失败！')
+}layer.msg('登陆成功！')
+            }
+        })
+        localStorage.setItem('token',res.token)
+    })
+    //注册表单提交
+    $('#reg-form').on('submit', function (e) {
+e.preventDefault();
+        $.post('http://www.liulongbin.top:3007/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                layer.msg('注册成功！')
+                $('#link_login').click()
+    }layer.msg('注册失败！')
+}
+
+)
+    })
+})
